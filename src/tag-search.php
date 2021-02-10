12857ae9d996fab60500b2ea3e5ec5ce719c3f0f
Chad Chadbourne <chad@automattic.com>
2021-02-10 18:55:12 -0500

    Added tag-search.php

--- src/tag-search.php
+++ src/tag-search.php
@@ -2,25 +2,23 @@
 /**
  * The GIF search script
  *
- * Powers the GIF script filter, and returns both gifs and tags
+ * Powers the gif script filter
  *
  * @since 2.0
  */
-
 require_once ( 'functions.php' );
 
 // Initiate a new query
 $query = new GIF_Query();
 
-//The GIF loop!
-// Imitation is the sincerest form of flattery
+//The tag loop!
 if ( $query->have_gifs() || $query->have_tags() ) {
 
 	echo '<?xml version="1.0"?>';
 	echo '<items>';
-	
-		while ( $query->have_gifs() ) {
-			$query->the_gif();
+
+		while ( $query->have_tags() ) {
+			$query->the_tag();
 		}
 
 	echo '</items>';
