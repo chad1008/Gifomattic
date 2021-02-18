3c582e94b5fcc9e57f02fcd4537ffc8b3e1df8f2
Chad Chadbourne <chad@automattic.com>
2021-02-17 22:04:17 -0500

    added success message to script output, introduced the tag_edit_mode env var

--- src/gif-save.php
+++ src/gif-save.php
@@ -9,7 +9,7 @@ require_once( 'functions.php' );
 
 // Gather details of the GIF to be added/updated. ID is optional, as edited GIFs will provide one, newly added GIFs will not
 $gif = array(
-	'id'   => !getenv( 'edited_gif_id' ) ? '' : getenv( 'edited_gif_id' ),
+	'id'   => !getenv( 'edit_gif_id' ) ? '' : getenv( 'edit_gif_id' ),
 	'url'  => getenv( 'gif_url' ),
 	'name' => getenv( 'gif_name' ),
 	'date' => date( 'F d, Y' ),
@@ -32,11 +32,22 @@ $result = $stmt->execute();
 // Grab the ID of the new GIF and add it to our array of the GIFs data
 $gif['id'] = $db->lastInsertRowID();
 
-
 // Create/update the icon for the GIF that was just added/updated
 iconify( $gif );
 
 $success = "GIF saved: " . $gif['name'];
-popup_notice( $success );
+$notification = popup_notice( $success );
 
+$output = array (
+	'alfredworkflow' => array(
+		'arg' => '',
+		'variables' => array(
+			'edit_gif_id'   => $gif['id'],
+			'notification'  => $notification,
+			'tag_edit_mode' => 'add_tags',
+		),
+	),
+);
+
+echo json_encode( $output );
 
