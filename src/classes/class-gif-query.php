dc59690a555d5a8cc0e4dbe360dec3f3b5ebe24a
Chad Chadbourne <chad@automattic.com>
2021-03-16 06:54:43 -0400

    apply coding standards

--- src/classes/class-gif-query.php
+++ src/classes/class-gif-query.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The GIF Query class.
  *
@@ -10,16 +11,17 @@ class GIF_Query {
 	 * Query vars
 	 *
 	 * @since 2.0
-	 * 
-	 * @var $input string The Alfred input query
-	 * @var $query string Custom query string based on the parameters provided
-	 * @var $tag_to_search int An optional, specific tag that query results should be filtered against
+	 *
+	 * @var $input          string The Alfred input query
+	 * @var $query          string Custom query string based on the parameters provided
+	 * @var $tag_to_search  int An optional, specific tag that query results should be filtered against
 	 * @var $is_trash_query bool Determines if the query should return trashed or untrashed GIFs. Defaults to FALSE
-	 * @var $cleanup int Determines if the query should only return GIFs ready for automatic cleanup. Defaults to FALSE
+	 * @var $cleanup        int Determines if the query should only return GIFs ready for automatic cleanup. Defaults
+	 *      to FALSE
 	 */
-	public  $input;
+	public $input;
 	private $query;
-	public  $tag_to_search;
+	public $tag_to_search;
 	private $is_trash_query;
 	private $cleanup;
 
@@ -63,18 +65,18 @@ class GIF_Query {
 	 *
 	 * @since 2.0
 	 *
-	 * @param mixed $input Alfred user input
-	 * @param int   $tag Optional tag to filter GIFs from
-	 * @param bool  $trash Set if the query should return trashed or untrashed GIFs
+	 * @param mixed $input   Alfred user input
+	 * @param int   $tag     Optional tag to filter GIFs from
+	 * @param bool  $trash   Set if the query should return trashed or untrashed GIFs
 	 * @param bool  $cleanup Set if the query should return GIFs ready for automatic removal from trash
 	 */
-	public function __construct( $input, $tag = null, $trash = FALSE, $cleanup = FALSE ) {
+	public function __construct( $input, $tag = null, $trash = false, $cleanup = false ) {
 		// Store query and its properties
-		$this->input		  = $input;
-		$this->tag_to_search  = $tag;
+		$this->input = $input;
+		$this->tag_to_search = $tag;
 		$this->is_trash_query = $trash;
-		$this->cleanup		  = $cleanup;
-		$this->query 		  = $this->parse_query();
+		$this->cleanup = $cleanup;
+		$this->query = $this->parse_query();
 
 		// Initialize counts
 		$this->current_gif = -1;
@@ -121,7 +123,7 @@ class GIF_Query {
 		$query .= " in_trash = :trashed";
 
 		// If cleanup mode is active, check the trashed dates
-		if ($this->cleanup == TRUE ) {
+		if ( $this->cleanup == true ) {
 			$query .= " AND trash_date < strftime('%s','now','-30 days')";
 		}
 
@@ -140,7 +142,7 @@ class GIF_Query {
 	public function get_gifs() {
 		$stmt = $this->db->prepare( $this->query );
 		$args = array(
-			':tag'	   => $this->tag_to_search,
+			':tag'     => $this->tag_to_search,
 			':input'   => $this->input,
 			':trashed' => $this->is_trash_query == TRUE ? 1 : 0, // 0 and 1 act as a boolean value for 'trashed' status
 		);
@@ -181,9 +183,9 @@ class GIF_Query {
 	 */
 	public function have_gifs() {
 		if ( $this->current_gif + 1 < $this->gif_count ) {
-			return TRUE;
+			return true;
 		} else {
-			return FALSE;
+			return false;
 		}
 	}
 
@@ -197,7 +199,7 @@ class GIF_Query {
 	public function the_gif() {
 		// Increment the current_gif pointer, and then use it to identify the current gif from the GIF array
 		++$this->current_gif;
-		$current_gif = $this->gifs[$this->current_gif]['gif_id'];
+		$current_gif = $this->gifs[ $this->current_gif ]['gif_id'];
 
 		// Populate GIF data into an array for eventual output as JSON for Alfred
 		$the_gif = new GIF( $current_gif );
@@ -213,6 +215,6 @@ class GIF_Query {
 	public function random() {
 		$rand = array_rand( $this->gifs );
 
-		return $this->gifs[$rand]['gif_id'];
+		return $this->gifs[ $rand ]['gif_id'];
 	}
 }
